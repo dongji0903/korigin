29db608edccc06f766a940b85e6700c8b8ecb0dc
LGM <LGM@Lenovo-laptop>
2018-03-12 15:16:36 +0900

    delete all

--- sphinxStore4/src/main/java/com/sphinx/korigin/shopping/product/dao/SupplierDao.java
+++
@@ -1,9 +0,0 @@
-package com.sphinx.korigin.shopping.product.dao;
-
-import com.sphinx.korigin.shopping.product.domain.Supplier;
-
-public interface SupplierDao {
-	public void addSupplier(Supplier supplier);
-	
-	public Supplier findSupplierById(String supplierId);
-}
--- sphinxStore4/src/main/webapp/WEB-INF/views/korigin/study/js/funtion.js
+++
@@ -1,4 +0,0 @@
-function openWindow(url) {
-	window.open(url,"_blank_1",
-						"toolbar=no,menubar=no,scrollbars=yes,resizable=no, width=450, height=200");
-}
